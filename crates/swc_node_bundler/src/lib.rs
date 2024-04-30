a1c66cb5b3452fa38a9fe3b4873d2e3c87736d8f
Sharad Chand <pepsighan@sharadchand.com>
2024-04-30 16:48:31 +0530

    No need for v1 or v2

--- crates/swc_node_bundler/src/lib.rs
+++ crates/swc_node_bundler/src/lib.rs
@@ -3,14 +3,14 @@
 #[cfg(test)]
 extern crate test;
 
-/// Explicit extern crate to use allocator.
-extern crate swc_malloc;
+// /// Explicit extern crate to use allocator.
+// extern crate swc_malloc;
 
 pub mod loaders;
-#[cfg(feature = "swc_v1")]
-pub mod v1;
-#[cfg(feature = "swc_v2")]
-pub mod v2;
+// #[cfg(feature = "swc_v1")]
+// pub mod v1;
+// #[cfg(feature = "swc_v2")]
+// pub mod v2;
 
-#[cfg(all(not(feature = "swc_v1"), not(feature = "swc_v2")))]
-compile_error!("Please enable swc_v1 or swc_v2 feature");
+// #[cfg(all(not(feature = "swc_v1"), not(feature = "swc_v2")))]
+// compile_error!("Please enable swc_v1 or swc_v2 feature");
